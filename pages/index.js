f4e44196126f5b2fab5f5bd88b384e14c84880bd
Oliveira-Lucas1379 <105256226+Oliveira-Lucas1379@users.noreply.github.com>
2023-07-08 03:19:07 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Trocando pra testar denovo, lembra de trocar novamente depois pra colocar uma frase maneira</h1>
+    return <h1>Te amo mozão, vamo ver um filmeco?? </h1>
 }
 
 export default Home;
