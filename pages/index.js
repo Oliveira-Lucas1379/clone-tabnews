17ec7a8165606ceefe8dce8e9f1d2a892dd724cc
Oliveira-Lucas1379 <105256226+Oliveira-Lucas1379@users.noreply.github.com>
2023-07-20 03:18:58 +0000

    adiciona scripts `lint:check` e `lint:write`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Teste forçando ammend</h1>
+  return <h1>Teste forçando ammend</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
 }
 
 export default Home;
