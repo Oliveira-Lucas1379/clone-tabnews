f86036e236ecdd76b45c5c9050e7dfb4517ab597
Oliveira-Lucas1379 <105256226+Oliveira-Lucas1379@users.noreply.github.com>
2023-07-20 03:37:12 +0000

    adiciona o arquivo `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Teste forçando ammend</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("teste2");
-}
-
 export default Home;
