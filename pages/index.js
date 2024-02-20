d4979b2ae838333c7efe9c74344f3293dff5de8d
Oliveira-Lucas1379 <105256226+Oliveira-Lucas1379@users.noreply.github.com>
2024-02-20 03:35:43 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+function Home() {
+  return (
+    <body>
+      <h1>
+        Olá Lucas, espero que você tenha conseguido resolver aquele problemão
+        que estou pensando tanto sobre.
+      </h1>
+      <h1>
+        -Vá aproveitando que o curso.dev está sendo construído e utiliza isso
+        pra ter mais tempo pra estudar outras coisas.
+      </h1>
+      <h1>-Revise suas notas.</h1>
+      <h1>-Teste</h1>
+    </body>
+  );
+}
+
+export default Home;
